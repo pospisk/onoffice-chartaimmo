edaee191a0b49e048d9cacc6a1b8b15cc622ce76
Kristian Pospis <kristian.pospis@gmail.com>
2020-11-20 15:37:46 +0100

    add Realestate component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,92 @@
 <template>
   <div id="app">
     <Header />
+    <section class="hero__wrapper">
       <div class="container">
         <div class="row">
-          <div class="col-6">
-            <h1>Heading</h1>
+          <div class="col-12">
+            <div class="hero__title">
+              Wir haben Ihre <br>
+              Traum&shy;immobilie.
+            </div>
           </div>
-          <div class="col-6">
-            <h1>Heading</h1>
+        </div>
+        <form>
+          <div class="row">
+            <div class="col-12 col-lg-3">
+              <input class="hero__input" placeholder="Ort">
+            </div>
+            <div class="col-12 col-lg-3">
+              <select class="hero__input">
+                <option value="" disabled selected>Immobilienart</option>
+                <option value="1">Büro</option>
+                <option value="2">Haus</option>
+                <option value="3">Wohnung</option>
+              </select>
+            </div>
+            <div class="col-12 col-lg-3">
+              <input class="hero__input" placeholder="Fläche ab">
+            </div>
+            <div class="col-12 col-lg-3">
+              <input class="hero__input" placeholder="Preis bis">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 d-flex justify-content-end">
+              <button class="hero__button">
+                Suchen
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+    <section class="textblock__wrapper pd__section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 offset-0 col-lg-8 offset-lg-2">
+            <div class="textblock__inner">
+              <div class="textblock__title">
+                Willkommen bei Charta Immo – <br>
+                Ihr Immobilienmakler für Wohnimmobilien in Musterhausen
+              </div>
+              <div class="textblock__content">
+                <p>
+                  Allgeier Wohnbau – das ist ein Unternehmen mit Tradition und Weitsicht. Unser Familienbetrieb, 1972
+                  gegründet, vermittelt seit fast 50 Jahren Menschen in Immobilien. Unter der Leitung von
+                  Geschäftsführer Alexander Vonalt vermitteln wir nicht nur Bestandsimmobilien, sondern entwickeln und
+                  setzen auch Wohnprojekte in der Region Freiburg im Breisgau, Gundelfingen und Umgebung um.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </section>
+    <section class="projects__wrapper pd__section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="projects__title">
+              <span class="projects__title--top">
+                Projekte
+              </span>
+              <span class="projects__title--bottom">
+                Aktuelle Wohnprojekte
+              </span>
+            </div>
+            <div class="row">
+              <Realestate/>
+              <Realestate/>
+              <Realestate/>
+              <Realestate/>
+              <Realestate/>
+              <Realestate/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <Footer />
   </div>
 </template>
@@ -18,12 +94,14 @@
 <script>
 import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
+import Realestate from './components/Realestate.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Footer
+    Footer,
+    Realestate
   }
 }
 </script>
---
+++ src/components/Realestate.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="col-12 col-lg-4">
+        <div class="project__item">
+            <div class="row">
+                <div class="col-12">
+                    <div class="project__imageWrapper">
+                        <img class="project__image" src="./../assets/img/clarisse-croset-MET1RiKQRjg-unsplash.png" alt="">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6 bd__bottom bd__bottom--primary"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="project__inner col-12">
+
+                <div class="row mx-0">
+                    <div class="col-12">
+                        <div class="project__title">
+                            {{realestateTitle}}
+                        </div>
+                    </div>
+                </div>
+                <div class="row mx-0">
+                    <div class="col-6">
+                        <span class="project__detail">
+                            Objekt-Nr.
+                        </span>
+                        <span class="project__info">
+                            {{realestateNumber}}
+                        </span>
+                    </div>
+                    <div class="col-6">
+                        <span class="project__detail">
+                            Kaufpreis
+                        </span>
+                        <span class="project__info">
+                            {{realestatePrice}} €
+                        </span>
+                    </div>
+                </div>
+                <div class="row mx-0">
+                    <div class="col-6">
+                        <span class="project__detail">
+                            Wohnfläche
+                        </span>
+                        <span class="project__info">
+                            ca. {{realestateArea}} m<sup>2</sup>
+                        </span>
+                    </div>
+                    <div class="col-6">
+                        <span class="project__detail">
+                            Zimmer
+                        </span>
+                        <span class="project__info">
+                            {{realestateRooms}}
+                        </span>
+                    </div>
+                </div>
+                <div class="row mx-0 project__footer">
+                    <div class="col-6 d-flex flex-row">
+                        <a href="tel:000000000" class="project__button project__button--call">
+                            <span class="sr-only">Anrufen</span>
+                        </a>
+                        <a href="mailto:mail@chartaimmo.de" class="project__button project__button--mail">
+                            <span class="sr-only">E-Mail</span>
+                        </a>
+                        <a href="#" class="project__button project__button--chat">
+                            <span class="sr-only">Chat</span>
+                        </a>
+                    </div>
+                    <div class="col-6 text-right">
+                        <a class="project__link" href="#">mehr erfahren</a>
+                    </div>
+                </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Realestate',
+  data: function () {
+    return {
+      realestateTitle: 'Neubauprojekt: Allbach - Wohnen in Muhausen',
+      realestateNumber: 345623,
+      realestatePrice: '414.400,00',
+      realestateArea: 101,
+      realestateRooms: 4
+    }
+  }
+}
+</script>
