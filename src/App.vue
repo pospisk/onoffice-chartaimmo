9e5c255976843dd8d819586a1d279b2fb1c0f9af
Kristian Pospis <kristian.pospis@gmail.com>
2020-11-20 10:51:23 +0100

    Revert "add bootstrap-vue"
    This reverts commit e182c69589442db2d04408fb631622339cfe3c07.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="app" class="container">
-    <div class="row">
-      <div class="col-6">
-        <h1>Heading</h1>
-      </div>
-      <div class="col-6">
-        <h1>Heading</h1>
-      </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    HelloWorld
   }
 }
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
